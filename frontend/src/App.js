f1a4135c4795ebed053018dff89b2bf2317d5bc2
portals <theodor.ang.98@gmail.com>
2018-05-27 16:46:10 +0200

    Added docker and removed react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,13 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
-import HomeScreen from './components/screens/homeScreen/index'
-import data from './service.json'
-
-
+import React, { Component } from "react";
+import HomeScreen from "./components/screens/homeScreen/index";
+import data from "./service.json";
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
-
-				<Switch>
-					<Route path="/" exact><HomeScreen services={data} /></Route>
-
-          {/* <Route path="/json" exact>{data}</Route> */}
-
-				</Switch>   
-
-			</div>
+        <HomeScreen services={data} />
+      </div>
     );
   }
 }
