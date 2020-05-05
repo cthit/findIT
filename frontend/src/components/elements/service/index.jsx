f7ea23e0f09fc272c4c21e7772b1a40974a3826f
Hoidi <tobiakar@student.chalmers.se>
2020-05-05 14:46:27 +0200

    Upgrade to rdc 1.0.1

--- frontend/src/components/elements/service/index.jsx
+++ frontend/src/components/elements/service/index.jsx
@@ -1,57 +1,58 @@
 import React from "react";
 import "font-awesome/css/font-awesome.min.css";
-import { ServiceIcon } from "./styles";
+import {ServiceIcon} from "./styles";
 import {
     DigitDesign,
     DigitButton,
     DigitText,
     DigitLayout,
     DigitMarkdown,
-    DigitIfElseRendering,
 } from "@cthit/react-digit-components";
 
+function githubButton(github_url) {
+    if (github_url !== "") {
+        return <a
+            style={{textDecoration: "none"}}
+            href={github_url}
+            target={"_blank"}
+        >
+            <DigitButton primary text={"GitHub"}/>
+        </a>
+    } else {
+        return <></>
+    }
+}
+
 export const Service = ({
-    title,
-    url,
-    description,
-    github_url,
-    fontawesome_icon,
-}) => (
-    <DigitDesign.Card width="320px" height="190px">
+                            title,
+                            url,
+                            description,
+                            github_url,
+                            fontawesome_icon,
+                        }) => (
+    <DigitDesign.Card size={{width: "300px", height: "250px"}}>
         <DigitDesign.CardBody>
             <DigitLayout.Row>
-                <ServiceIcon className={"fa " + fontawesome_icon} />
+                <ServiceIcon className={"fa " + fontawesome_icon}/>
                 <a
-                    style={{ color: "black", textDecoration: "none" }}
+                    style={{color: "black", textDecoration: "none"}}
                     href={url}
                     target={"_blank"}
                 >
-                    <DigitText.Heading5 text={title} bold />
+                    <DigitText.Heading5 text={title} bold/>
                 </a>
             </DigitLayout.Row>
-            <DigitMarkdown markdownSource={description} />
+            <DigitMarkdown markdownSource={description}/>
         </DigitDesign.CardBody>
         <DigitDesign.CardButtons reverseDirection>
             <a
-                style={{ marginLeft: "10px", textDecoration: "none" }}
+                style={{marginLeft: "10px", textDecoration: "none"}}
                 href={url}
                 target={"_blank"}
             >
-                <DigitButton primary outlined raised text={"Open service"} />
+                <DigitButton primary outlined raised text={"Open service"}/>
             </a>
-            <DigitIfElseRendering
-                test={github_url !== ""}
-                ifRender={() => (
-                    <a
-                        style={{ textDecoration: "none" }}
-                        href={github_url}
-                        target={"_blank"}
-                    >
-                        <DigitButton primary text={"GitHub"} />
-                    </a>
-                )}
-                elseRender={() => <div></div>}
-            />
+            {githubButton(github_url)}
         </DigitDesign.CardButtons>
     </DigitDesign.Card>
 );
