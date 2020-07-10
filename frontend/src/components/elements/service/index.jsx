1be54eadf198db07c050938388d7032ccc4af426
Hoidi <tobiakar@student.chalmers.se>
2020-07-10 12:46:59 +0200

    rdc 1.0.2 and prettier

--- frontend/src/components/elements/service/index.jsx
+++ frontend/src/components/elements/service/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "font-awesome/css/font-awesome.min.css";
-import {ServiceIcon} from "./styles";
+import { ServiceIcon } from "./styles";
 import {
     DigitDesign,
     DigitButton,
@@ -11,49 +11,51 @@ import {
 
 function githubButton(github_url) {
     if (github_url !== "") {
-        return <a
-            style={{textDecoration: "none"}}
-            href={github_url}
-            target={"_blank"}
-            rel="noopener noreferrer"
-        >
-            <DigitButton primary text={"GitHub"}/>
-        </a>
+        return (
+            <a
+                style={{ textDecoration: "none" }}
+                href={github_url}
+                target={"_blank"}
+                rel="noopener noreferrer"
+            >
+                <DigitButton primary text={"GitHub"} />
+            </a>
+        );
     } else {
         return null;
     }
 }
 
 export const Service = ({
-                            title,
-                            url,
-                            description,
-                            github_url,
-                            fontawesome_icon,
-                        }) => (
-    <DigitDesign.Card size={{width: "300px", height: "250px"}}>
+    title,
+    url,
+    description,
+    github_url,
+    fontawesome_icon,
+}) => (
+    <DigitDesign.Card size={{ width: "300px", height: "250px" }}>
         <DigitDesign.CardBody>
             <DigitLayout.Row>
-                <ServiceIcon className={"fa " + fontawesome_icon}/>
+                <ServiceIcon className={"fa " + fontawesome_icon} />
                 <a
-                    style={{color: "black", textDecoration: "none"}}
+                    style={{ color: "black", textDecoration: "none" }}
                     href={url}
                     target={"_blank"}
                     rel="noopener noreferrer"
                 >
-                    <DigitText.Heading5 text={title} bold/>
+                    <DigitText.Heading5 text={title} bold />
                 </a>
             </DigitLayout.Row>
-            <DigitMarkdown markdownSource={description}/>
+            <DigitMarkdown markdownSource={description} />
         </DigitDesign.CardBody>
         <DigitDesign.CardButtons reverseDirection>
             <a
-                style={{marginLeft: "10px", textDecoration: "none"}}
+                style={{ marginLeft: "10px", textDecoration: "none" }}
                 href={url}
                 target={"_blank"}
                 rel="noopener noreferrer"
             >
-                <DigitButton primary outlined raised text={"Open service"}/>
+                <DigitButton primary outlined raised text={"Open service"} />
             </a>
             {githubButton(github_url)}
         </DigitDesign.CardButtons>
