7f193134c047c5e518e076f6f5a493cee6fddfb6
x183 <palmoscar4@gmail.com>
2022-08-25 13:48:53 +0200

    stylish icons everywhere

--- frontend/components/views/ServiceCard.tsx
+++ frontend/components/views/ServiceCard.tsx
@@ -7,29 +7,37 @@ import styles from "./ServiceCard.module.scss";
 export interface ServiceCardProps {
   service: Service;
 }
-const ServiceCard = ({ service }: ServiceCardProps) => (
-  <div className="card">
-    <p className={styles.cardTitle}>
-      <img
-        src={`${service.url}/favicon.ico`}
-        className={`${styles.iconClass} marginTop marginLeft marginRight`}
-        alt=" "
-      />
-      {service.title}
-    </p>
-    <p className="marginTop">{service.description}</p>
-    <a className={styles.aClass} href={service.github_url}>
-      GITHUB
-    </a>
-    <button
-      className={styles.buttonClass}
-      onClick={() => {
-        window.open(service.url);
-      }}
-    >
-      OPEN SERVICE
-    </button>
-  </div>
-);
+function ServiceCard({ service }: ServiceCardProps) {
+  let src = "images/";
+  if (service.icon) {
+    src += service.icon+".svg";
+  } else {
+    src = service.url + "/favicon.ico";
+  }
+  return (
+    <div className="card">
+      <p className={styles.cardTitle}>
+        <img
+          src={src}
+          className={`${styles.iconClass} marginTop marginLeft marginRight`}
+          alt=" "
+        />
+        {service.title}
+      </p>
+      <p className="marginTop">{service.description}</p>
+      <a className={styles.aClass} href={service.github_url}>
+        GITHUB
+      </a>
+      <button
+        className={styles.buttonClass}
+        onClick={() => {
+          window.open(service.url);
+        }}
+      >
+        OPEN SERVICE
+      </button>
+    </div>
+  );
+}
 
 export default ServiceCard;
