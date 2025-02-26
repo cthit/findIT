a083f8736f6bbd94d3baadd8c4819273ad0fb36b
Simon Westlin Green <simon.green@live.se>
2025-02-26 17:32:04 +0100

    Removed a lot of unused packages (#68)

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,8 +2,6 @@ import "../resources/styles/Theme_colors.css";
 import "../resources/styles/globals.scss";
 import "../resources/styles/source_sans_pro.scss";
 import "../resources/styles/deja_vu_sans.scss";
-// import Font Awesome CSS
-import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import type { AppProps } from "next/app";
 
